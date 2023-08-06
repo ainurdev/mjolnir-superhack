70662073ba7d73068386eee87ea46c533276b33a
Mahziyar Erfani <mhzrerfani@gmail.com>
2023-08-06 08:02:07 +0330

    Add mjolnir logo

---
+++ web/src/pages/_components/Logo.svelte
@@ -0,0 +1,9 @@
+<script>
+  import logo from "/mjolnir.svg";
+</script>
+
+<div
+  class="w-12 flex items-center justify-center shrink-0 grow-0 h-12 rounded-xl bg-primary-400"
+>
+  <img src={logo} alt="Mjolnir Logo" />
+</div>
