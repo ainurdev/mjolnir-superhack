b90f2138b2a941d4df2ccb471f6806624fc3cc7a
Mahziyar Erfani <mhzrerfani@gmail.com>
2023-08-06 07:13:12 +0330

    Fix vite alias

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,18 +1,8 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import path from "path";
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
+
   preprocess: vitePreprocess(),
-  resolve: {
-    alias: {
-      "@/components": path.resolve("./src/pages/_components"),
-      "@/constants": path.resolve("./src/constants"),
-      "@/icons": path.resolve("./src/pages/_components/icons"),
-      "@/stores": path.resolve("./src/stores"),
-      "@/types": path.resolve("./src/types"),
-      "@/utils": path.resolve("./src/utils"),
-    },
-  },
 };
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  resolve: {
+    alias: {
+      "@/icons": path.resolve("./src/pages/_components/icons"),
+      "@/constants": path.resolve("./src/constants"),
+      "@/stores": path.resolve("./src/stores"),
+      "@/types": path.resolve("./src/types"),
+      "@/utils": path.resolve("./src/utils"),
+      "@/pages": path.resolve("./src/pages"),
+    },
+  },
+});
