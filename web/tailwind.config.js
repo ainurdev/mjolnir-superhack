bc877d5a47d28f6a8355cabf7ebbbfe2ea007fbe
Mahziyar Erfani <mhzrerfani@gmail.com>
2023-08-06 08:01:06 +0330

    Add primary pallette

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.blue,
+      },
+    },
   },
   plugins: [],
 };
