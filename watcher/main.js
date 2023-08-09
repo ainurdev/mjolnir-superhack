c466a55fa2fec89c0f8918cbc0ec090eff271177
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2023-08-09 23:22:54 +0330

    Add helia

--- watcher/main.js
+++ watcher/main.js
@@ -6,15 +6,31 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, './pages/preload.js')
+      preload: path.join(__dirname, './pages/preload.js'),
+      nodeIntegration: true
     }
   })
 
   win.loadFile(path.join(__dirname, './pages/index.html'))
+
+  if (process.env.NODE_ENV !== 'production') {
+    win.webContents.openDevTools()
+  }
 }
 
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
   createWindow()
+
+  try {
+    // Helia is an ESM-only module but Electron currently only supports CJS
+    // at the top level, so we have to use dynamic imports to load it
+    const { createNode } = await import('./helia.mjs')
+    const node = await createNode()
+    const id = node.libp2p.peerId
+    console.log(id)
+  } catch (err) {
+    console.error(err)
+  }
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
