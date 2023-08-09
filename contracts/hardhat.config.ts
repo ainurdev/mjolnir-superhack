d2595fd3655af74b257ded36f012432417f5d47c
Arman Mazdaee <arman@ainur.dev>
2023-08-10 00:06:19 +0330

    Add deploy script for contracts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,9 +1,21 @@
 import '@nomicfoundation/hardhat-toolbox';
 import '@nomiclabs/hardhat-solhint';
+import { ethers } from 'hardhat';
 import { HardhatUserConfig } from 'hardhat/config';
+
+const privateKey = process.env.PRIVATE_KEY;
+if (privateKey === undefined) {
+  throw Error('PRIVATE_KEY env is not set');
+}
 
 const config: HardhatUserConfig = {
   solidity: '0.8.19',
+  networks: {
+    goerli: {
+      url: 'https://ethereum-goerli.publicnode.com',
+      accounts: [privateKey],
+    },
+  },
 };
 
 export default config;
