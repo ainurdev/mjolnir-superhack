8b478b1a6a38b6285347238af27fd64dc2535395
mhzrerfani <mhzrerfani@gmail.com>
2023-08-13 12:38:54 +0330

    Add get metadata methods

--- web/src/queries/index.ts
+++ web/src/queries/index.ts
@@ -1,10 +1,22 @@
-import { gql } from "@urql/svelte";
+import { gql } from '@urql/svelte';
 
 export const getStations = gql`
-  query($where: Station_filter) {
+  query ($where: Station_filter) {
     stations(where: $where) {
       id
       owner
     }
   }
-`
+`;
+
+export const getStationMEtadata = async cid => {
+  const response = await fetch(`https://ipfs.io/ipfs/${cid}/metadata.json`);
+  const data = await response.json();
+  return data;
+};
+
+export const getImage = async cid => {
+  const response = await fetch(`https://ipfs.io/ipfs/${cid}`);
+  const data = await response.blob();
+  return data;
+};
