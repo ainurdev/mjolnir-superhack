63504416238a5ef5ef50e25a69054d2921155940
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2023-08-13 18:41:20 +0330

    Fix chainid

--- web/src/constants/index.ts
+++ web/src/constants/index.ts
@@ -3,11 +3,11 @@ export const BASE_URL = 'https://gettv-srs.testing.gettv.ainur.dev';
 export const LOCALSTORAGE_KEY = 'mjolnir-superhack-';
 
 export const registry = {
-  919: {
+  '0x1a4': {
     stations: '0x9d6a427c92F91A10e8F5fd737656856Cb98b3a3b',
     subscriptions: '0x7fbCefa5167EB2f305ad5B70D89123608e2D3dED',
   },
-  84531: {
+  '0x14a33': {
     stations: '0x9d6a427c92F91A10e8F5fd737656856Cb98b3a3b',
     subscriptions: '0x7fbCefa5167EB2f305ad5B70D89123608e2D3dED',
   },
